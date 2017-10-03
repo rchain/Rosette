0dcb3cdc9fc0b105e3916366b0bf82fbf6d0fcba
Alexander <dosofredriver@gmail.com>
2017-10-03 23:22:41 +0600

    Add `lit` method to the `Code` class

--- rosette-scala/src/main/scala/coop/rchain/rosette/Code.scala
+++ rosette-scala/src/main/scala/coop/rchain/rosette/Code.scala
@@ -1,5 +1,8 @@
 package coop.rchain.rosette
 
-case class Code(override val meta: Ob, override val slot: Seq[Ob]) extends Ob {
-  def lit(l: Int): Ob = null
+case class Code(litvec: Tuple,
+                override val meta: Ob,
+                override val slot: Seq[Ob])
+    extends Ob {
+  def lit(l: Int): Ob = litvec.elem(l)
 }
