b361ea706afac9550b0c3816b71a12f6ec737007
Timm Schäuble <timm.schaeuble@gmail.com>
2017-10-04 00:11:08 +0200

    Remove cruft

--- rosette-scala/src/main/scala/coop/rchain/rosette/Main.scala
+++ rosette-scala/src/main/scala/coop/rchain/rosette/Main.scala
@@ -5,31 +5,10 @@ import java.io.InputStreamReader
 import coop.rchain.rosette.parser.bytecode._
 import Show._
 
+import scala.collection.mutable
+
 object Main extends App {
-
-  /*
-  // TODO: Remove before merge
-  val test = Seq[Int](
-    Integer.parseInt("00000000", 2), // halt
-    Integer.parseInt("00000001", 2), // ignored arg
-    Integer.parseInt("00000011", 2), // nargs
-    Integer.parseInt("00101010", 2), // nargs arg
-    Integer.parseInt("00001011", 2), // outstanding
-    Integer.parseInt("00000000", 2), // outstanding p arg
-    Integer.parseInt("00000000", 2), // outstanding n arg
-    Integer.parseInt("00001000", 2), // outstanding x arg
-  )
-
-  def writeFile(filename: String, bytes: Seq[Int]): Unit = {
-    val bos = new BufferedOutputStream(new FileOutputStream(filename))
-    bos.write(bytes.map(_.toByte).toArray)
-    bos.close()
-  }
-
-  writeFile("test.txt", test)
-   */
-
-  val bytes = collection.mutable.ArrayBuffer[Int]()
+  val bytes = mutable.ArrayBuffer[Int]()
   val stdin = new InputStreamReader(System.in)
 
   Stream.continually(stdin.read()).takeWhile(_ != -1).foreach(bytes += _)
